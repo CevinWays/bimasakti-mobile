ad7fbae969cf386fc7515f179e58686e773fc863
CevinWays <cevinways34@gmail.com>
2024-07-20 11:14:28 +0700

    add splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'dart:io';
 
-import 'package:device_info_plus/device_info_plus.dart';
+import 'package:bimasakti/splash_page.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,226 +25,13 @@ void main() async {
       );
 
   runApp(
-    MaterialApp(
+    GetMaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 58, 66, 183)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Bimasakti'),
+      home: const SplashPage(),
     ),
   );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey webViewKey = GlobalKey();
-
-  InAppWebViewController? webViewController;
-  InAppWebViewSettings settings = InAppWebViewSettings(
-      isInspectable: kDebugMode,
-      mediaPlaybackRequiresUserGesture: false,
-      allowsInlineMediaPlayback: true,
-      iframeAllow: "camera; microphone",
-      iframeAllowFullscreen: true,
-      allowsBackForwardNavigationGestures: true,
-      useOnDownloadStart: true);
-
-  PullToRefreshController? pullToRefreshController;
-  String url = "";
-  double progress = 0;
-  final urlController = TextEditingController();
-  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    pullToRefreshController = kIsWeb
-        ? null
-        : PullToRefreshController(
-            settings: PullToRefreshSettings(
-              color: const Color.fromARGB(255, 58, 66, 183),
-            ),
-            onRefresh: () async {
-              if (defaultTargetPlatform == TargetPlatform.android) {
-                webViewController?.reload();
-              } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-                webViewController?.loadUrl(
-                    urlRequest:
-                        URLRequest(url: await webViewController?.getUrl()));
-              }
-            },
-          );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: getListUrl(url) ? null : buildAppBar(context),
-      body: buildBody(context),
-    );
-  }
-
-  PreferredSizeWidget buildAppBar(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () async {
-            final canGoback = await webViewController?.canGoBack() ?? false;
-            if (canGoback) {
-              webViewController?.goBack();
-            }
-          }),
-    );
-  }
-
-  Widget buildBody(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Stack(
-              children: [
-                InAppWebView(
-                    key: webViewKey,
-                    initialUrlRequest:
-                        URLRequest(url: WebUri("https://bsi.ciapps.id/")),
-                    initialSettings: settings,
-                    pullToRefreshController: pullToRefreshController,
-                    onWebViewCreated: (controller) {
-                      webViewController = controller;
-                    },
-                    onLoadStart: (controller, url) {
-                      setState(() {
-                        this.url = url.toString();
-                        urlController.text = this.url;
-                      });
-                    },
-                    onPermissionRequest: (controller, request) async {
-                      return PermissionResponse(
-                          resources: request.resources,
-                          action: PermissionResponseAction.GRANT);
-                    },
-                    shouldOverrideUrlLoading:
-                        (controller, navigationAction) async {
-                      var uri = navigationAction.request.url!;
-
-                      if (![
-                        "http",
-                        "https",
-                        "file",
-                        "chrome",
-                        "data",
-                        "javascript",
-                        "about"
-                      ].contains(uri.scheme)) {
-                        if (await canLaunchUrl(uri)) {
-                          // Launch the App
-                          await launchUrl(
-                            uri,
-                          );
-                          // and cancel the request
-                          return NavigationActionPolicy.CANCEL;
-                        }
-                      }
-
-                      return NavigationActionPolicy.ALLOW;
-                    },
-                    onLoadStop: (controller, url) async {
-                      pullToRefreshController?.endRefreshing();
-                      setState(() {
-                        this.url = url.toString();
-                        urlController.text = this.url;
-                      });
-                    },
-                    onReceivedError: (controller, request, error) {
-                      pullToRefreshController?.endRefreshing();
-                    },
-                    onProgressChanged: (controller, progress) {
-                      if (progress == 100) {
-                        pullToRefreshController?.endRefreshing();
-                      }
-                      setState(() {
-                        this.progress = progress / 100;
-                        urlController.text = url;
-                      });
-                    },
-                    onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                      setState(() {
-                        this.url = url.toString();
-                        urlController.text = this.url;
-                      });
-                    },
-                    onConsoleMessage: (controller, consoleMessage) {
-                      if (kDebugMode) {
-                        print(consoleMessage);
-                      }
-                    },
-                    onDownloadStartRequest:
-                        (controller, downloadStartRequest) async {
-                      var permissionStatus;
-
-                      if (await isBelowAndroid32(deviceInfo)) {
-                        permissionStatus = await Permission.storage.request();
-                      } else {
-                        permissionStatus =
-                            await Permission.photos.request();
-                      }
-
-                      if (permissionStatus == PermissionStatus.granted) {
-                        final directory = (Platform.isAndroid)
-                            ? await getExternalStorageDirectory()
-                            : await getApplicationDocumentsDirectory();
-
-                        String fileExtension = url.split('.').last;
-                        String filename = '${DateTime.now().millisecondsSinceEpoch}.$fileExtension';
-
-                        final taskId = await FlutterDownloader.enqueue(
-                          fileName: downloadStartRequest.suggestedFilename ?? filename,
-                          //TODO : ulater use downloadStartRequest.url
-                          url: url,
-                          savedDir: directory?.path ?? '',
-                          saveInPublicStorage: true,
-                          showNotification:
-                              true, // show download progress in status bar (for Android)
-                          openFileFromNotification:
-                              true, // click on notification to open downloaded file (for Android)
-                        );
-                        if (kDebugMode) {
-                          print('onDownloadStart $url');
-                          print('onDownload taskId: $taskId');
-                        }
-                      }
-                    }),
-                progress < 1.0
-                    ? LinearProgressIndicator(value: progress)
-                    : Container(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  bool getListUrl(String url) {
-    final listUrl = [
-      "https://bsi.ciapps.id/",
-      "https://bsi.ciapps.id/page/dashboard",
-    ];
-    return listUrl.contains(url);
-  }
-
-  static Future<bool> isBelowAndroid32(DeviceInfoPlugin deviceInfo) async {
-    final androidInfo = await deviceInfo.androidInfo;
-    return androidInfo.version.sdkInt < 32;
-  }
 }
